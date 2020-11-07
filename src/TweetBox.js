571b663c1ca39b62881c670c5b481b72a1b93f97
Amaan56 <shaikhamaan257@gmail.com>
2020-11-08 04:17:31 +0530

    Twitter box

---
+++ src/TweetBox.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Avatar, Button } from '@material-ui/core';
+import './TweetBox.css';
+
+function TweetBox() {
+  return (
+    <div class="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+          <input placeholder="What's happening?" type="text" />
+        </div>
+        <input
+          className="tweetBox__imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+        <Button className="tweetBox__tweetButton">Tweet</Button>
+      </form>
+    </div>
+  );
+}
+
+export default TweetBox;
