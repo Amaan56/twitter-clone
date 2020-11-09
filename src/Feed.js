df03855c07b6cbaadf5ba9968ede849cc9d3ee0e
Amaan56 <shaikhamaan257@gmail.com>
2020-11-10 03:04:59 +0530

    Post UI

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Feed.css';
 import TweetBox from './TweetBox';
+import Post from './Post';
 
 function Feed() {
   return (
@@ -9,6 +10,8 @@ function Feed() {
         <h2>Home</h2>
       </div>
       <TweetBox />
+      <Post />
+      <Post />
       <Post />
     </div>
   );
--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,47 @@
 import React from 'react';
 import './Post.css';
+import { Avatar } from '@material-ui/core';
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import PublishIcon from '@material-ui/icons/Publish';
 
 function Post() {
-  return <div></div>;
+  return (
+    <div className="post">
+      <div className="post__avatar">
+        <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png" />
+      </div>
+      <div className="post__body">
+        <div className="post__header">
+          <div className="post__headerText">
+            <h3>
+              Akshar
+              <span className="post__headerSpecial">
+                <VerifiedUserIcon className="post__badge" /> @AksharPathak
+              </span>
+            </h3>
+          </div>
+          <div className="post__headerDescription">
+            President-elect Biden on COVID: "I implore you... wear a mask... A
+            mask is not a political statement."
+          </div>
+          <img
+            className="post__image"
+            src="https://pbs.twimg.com/media/EmZYpOjVkAARvCV?format=jpg&name=large"
+            alt="Image"
+          />
+        </div>
+        <div className="post__footer">
+          <ChatBubbleOutlineIcon fontSize="small" />
+          <RepeatIcon fontSize="small" />
+          <FavoriteBorderIcon fontSize="small" />
+          <PublishIcon fontSize="small" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Post;
