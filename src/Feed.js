99ea78497252a214dc13d8b505becdd6ca6716cf
Amaan56 <shaikhamaan257@gmail.com>
2020-11-11 02:49:14 +0530

    Fliping post animation

--- src/Feed.js
+++ src/Feed.js
@@ -3,6 +3,7 @@ import './Feed.css';
 import TweetBox from './TweetBox';
 import Post from './Post';
 import db from './firebase';
+import FlipMove from 'react-flip-move';
 
 function Feed() {
   const [posts, setPosts] = useState([]);
@@ -19,17 +20,19 @@ function Feed() {
         <h2>Home</h2>
       </div>
       <TweetBox />
-      {posts.map((post) => (
-        <Post
-          key={post.text}
-          displayName={post.displayName}
-          username={post.username}
-          verified={post.verified}
-          text={post.text}
-          avatar={post.avatar}
-          image={post.image}
-        />
-      ))}
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={post.text}
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+      </FlipMove>
     </div>
   );
 }
